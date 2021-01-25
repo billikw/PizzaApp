96a9ac575dadf74570d970efdae6f5ac6fedaafa
billikw <66752314+billikw@users.noreply.github.com>
2021-01-25 00:26:48 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
+"use strict";
 
-/* MODAL SELECTORS */
+/* SELECTORS */
+//Modals:
 let welcomeText = document.querySelector("#welcomeText");
 let sizeModal = document.querySelector("#size");
 let crustModal = document.querySelector("#crust");
+let toppingsModal = document.querySelector("#toppings");
 let optionsModal = document.querySelector("#options");
+//Buttons:
 let startButton = document.querySelector("#btnStart");
 let twelveInchButton = document.querySelector("#btn12inch");
 let fourteenInchButton = document.querySelector("#btn14inch");
-
+let thinButton = document.querySelector("#btnThin");
+let thickButton = document.querySelector("#btnThick");
 
 
 /* LISTENERS */
 startButton.addEventListener("click", revealModal("size"));
-twelveInchButton.addEventListener("click", revealModal);
-fourteenInchButton.addEventListener("click", revealModal);
+twelveInchButton.addEventListener("click", revealModal("crust",null,12));
+fourteenInchButton.addEventListener("click", revealModal("crust",null,14));
+thinButton.addEventListener("click", revealModal("toppings","thin"));
+thickButton.addEventListener("click", revealModal("toppings","thick"));
 
 
-/* PIZZA TEMPORARY VARIABLES */
-let crust;
-let size;
-let toppings;
-let extraOptions;
+/* PIZZA GLOBAL */
 var pizza;
 
 /* PIZZA BUILDER */
@@ -33,17 +36,51 @@ function pizzaObject(crust, size, toppings, extraOptions) {
 
 /* FUNCTIONS */
 
-function revealModal(modal) {
+function revealModal(modal, pizzaThickness, pizzaSize, pizzaToppings, pizzaOptions) {
+
     switch (modal) {
-        case "size": 
-        sizeModal.classList.remove("inactive")
-        pizza = new pizzaObject()
-        console.log('Loaded Size modal');
-        case "crust": crustModal.classList.remove("inactive"); console.log('Loaded Crust modal');
-        case "options": optionsModal.classList.remove("inactive");
-        default: console.log("No value sent! No action taken");
+
+        case "size":
+            sizeModal.classList.remove("inactive");
+            pizza = new pizzaObject();
+            console.log('Loading Size modal');
+            console.log(pizza);
+            break;
+
+        case "crust":
+            //Set size:
+            if (pizzaSize === 12){
+                pizza.size = pizzaSize;
+            } else pizza.size = 14;
+            //Hide the size modal:
+            sizeModal.classList.add("inactive");
+            //Display the crust modal:
+            crustModal.classList.remove("inactive");
+            console.log('Loading Crust modal');
+            console.log(pizza);
+            break;
+
+        case "toppings":
+            //Set base style:
+            if (pizzaThickness === "thin") {
+                pizza.crust = "thin";
+            } else pizza.crust = "thick";
+            //Hide the crust modal:
+            crustModal.classList.add("inactive");
+            //Display the toppings modal:
+            toppingsModal.classList.remove("inactive");
+            console.log(pizza);
+            break;
+
+        case "options":
+            optionsModal.classList.remove("inactive");
+            toppingsModal.classList.add("#inactive");
+            console.log('Loading Options modal');
+            console.log(pizza);
+            break;
+
+        default: console.log("No value sent! No action taken"); pizza = undefined; console.log(pizza);
     }
-    console.log('Loaded Crust modal');
 }
 
 /* DEFAULT STATES */
